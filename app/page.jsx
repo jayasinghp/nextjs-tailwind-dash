f19a4726ffabff4a062c5c2e5412efcb2a17bae0
jayasinghp <jayasinghp@yahoo.com>
2025-02-18 23:14:23 +0530

    initial cleaned code

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1> HomePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        voluptatum officia repellendus, animi doloremque, in dolorem architecto
+        rerum atque officiis nostrum aperiam ullam sequi porro dolorum corporis
+        sed corrupti expedita
+      </p>
+    </div>
+  );
+}
+
+export default HomePage
